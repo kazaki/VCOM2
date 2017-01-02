8f6e01eda34ec7be3ff790b23d599498576ec029
Pedro <pedro_fernandes@live.com>
2017-01-02 23:39:27 +0000

    old

--- Main.cpp
+++ Main.cpp
@@ -59,18 +59,11 @@ int main( int argc, char** argv ) {
 	clock_t begin = clock();
 	
 	// detecting keypoints
-	/*
-	SurfFeatureDetector detector; // very bad
-	SiftFeatureDetector detector2;
-	OrbFeatureDetector detector3; // very bad
-	FastFeatureDetector detector4;
-	DenseFeatureDetector detector5; // too much bad points
-	*/
-	FastFeatureDetector detector;
+	SurfFeatureDetector detector(400);
 	vector<KeyPoint> keypoints;	
 	
 	// computing descriptors
-	Ptr<DescriptorExtractor > extractor(new OpponentColorDescriptorExtractor(Ptr<DescriptorExtractor>(new SiftDescriptorExtractor())));
+	Ptr<DescriptorExtractor > extractor(new SurfDescriptorExtractor());//  extractor;
 	Mat descriptors;
 	Mat training_descriptors(1,extractor->descriptorSize(),extractor->descriptorType());
 	Mat img;
@@ -86,8 +79,6 @@ int main( int argc, char** argv ) {
 		return 0;
 	}
 
-	int empty_keypoints_c = 0;
-	
 	do {
 		const string file_name = file_data.cFileName;
 		const string full_file_name = train_img_dir + "/" + file_name;
@@ -100,57 +91,18 @@ int main( int argc, char** argv ) {
 			continue;
 
 		img = imread(full_file_name);
-
-		Mat img_g = imread(full_file_name, CV_LOAD_IMAGE_GRAYSCALE);
-		GaussianBlur( img_g, img_g, Size( 1, 1 ), 0, 0 );
-		
-		detector.detect(img_g, keypoints);
-
-		/*int detectors_vals[5];
-		string detectors[5] = {"SURF","SIFT","ORB","FAST","DENSE"};
-		//-- Draw keypoints
-		Mat ik1,ik2,ik3;
-		//detector.detect(img_g, keypoints);
-		//detectors_vals[0]=keypoints.size();
-		//cout << "SURF: " << keypoints.size() << endl;
-		detector2.detect(img_g, keypoints);
-		detectors_vals[1]=keypoints.size();
-		cout << "SIFT: " << keypoints.size() << endl;
-		drawKeypoints( img_g, keypoints, ik1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
-		//detector3.detect(img_g, keypoints);
-		//detectors_vals[2]=keypoints.size();
-		//cout << "ORB: " << keypoints.size() << endl;
-		detector4.detect(img_g, keypoints);
-		detectors_vals[3]=keypoints.size();
-		cout << "FAST: " << keypoints.size() << endl;
-		drawKeypoints( img_g, keypoints, ik2, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
-		detector5.detect(img_g, keypoints);
-		detectors_vals[4]=keypoints.size();
-		cout << "DENSE: " << keypoints.size() << endl;
-		drawKeypoints( img_g, keypoints, ik3, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
-		cout << "Best: " << detectors[distance(detectors_vals, max_element(detectors_vals,detectors_vals+5))] << endl << endl;
-		/*-- Show detected (drawn) keypoints
-		imshow("Sample", img);
-		imshow("SIFT", ik1 );
-		imshow("FAST", ik2 );
-		imshow("DENSE", ik3 );
-		waitKey();/**/
-
-		if(keypoints.empty()) {
-			empty_keypoints_c++;
-			//cout << "Unable to detect any keypoints for this image." << endl;
-			continue;
-		}
+		detector.detect(img, keypoints);
 		extractor->compute(img, keypoints, descriptors);
-		training_descriptors.push_back(descriptors);
 
-		cout << "Number of empty keypoints: " << empty_keypoints_c << "  Total descriptors: " << training_descriptors.rows << "\r" ;
+		training_descriptors.push_back(descriptors);
+		cout << '\r' << "Total descriptors: " << training_descriptors.rows;
+
 
 	} while (FindNextFile(dir, &file_data));
 
 	cout << endl << endl;
 
-	BOWKMeansTrainer bowtrainer(1000); //num clusters
+	BOWKMeansTrainer bowtrainer(15); //num clusters
 	bowtrainer.add(training_descriptors);
 	cout << "Clustering Bag of Words features..." << endl;
 	Mat vocabulary = bowtrainer.cluster();
@@ -158,15 +110,11 @@ int main( int argc, char** argv ) {
 	cout << endl;
 
 	Ptr<DescriptorMatcher > matcher(new BruteForceMatcher<L2<float> >());
-	//BOWImgDescriptorExtractor bowide(extractor,matcher);
-	//bowide.setVocabulary(vocabulary);
-	Ptr<BOWImgDescriptorExtractor> bowide(new BOWImgDescriptorExtractor(extractor,matcher));
-	bowide->setVocabulary(vocabulary);
-
+	BOWImgDescriptorExtractor bowide(extractor,matcher);
+	bowide.setVocabulary(vocabulary);
 
 	//setup training data for classifiers
-	map<string,Mat> classes_training_data;
-	vector<string> class_labels;
+	map<string,Mat> classes_training_data; 
 	classes_training_data.clear();
 
 	cout << "Training SVMs..." << endl;
@@ -190,12 +138,10 @@ int main( int argc, char** argv ) {
 
 		keypoints.clear();
 		detector.detect(img, keypoints);
-		//bowide.compute(img, keypoints, response_hist);
-		bowide->compute(img, keypoints, response_hist);
+		bowide.compute(img, keypoints, response_hist);
 		
 		if(classes_training_data.count(label) == 0) {
 			classes_training_data[label].create(0,response_hist.cols,response_hist.type());
-			class_labels.push_back(label);
 		}
 		classes_training_data[label].push_back(response_hist);
 
@@ -203,72 +149,49 @@ int main( int argc, char** argv ) {
 	cout << '\r' << "                                           " << endl;
 
 
+	cout << "Training classes..." << endl;
+
 	//train 1-vs-all SVMs
-	CvSVM svm;
-	CvSVMParams params = CvSVMParams(); params.svm_type = CvSVM::C_SVC;
 	map<string, Ptr<CvSVM>> classes_classifiers;
 	int num = 1;
-	
-	Mat samples(0,response_hist.cols,response_hist.type());
-	Mat labels(0,1,CV_32FC1);
-	Mat samples_32f; 
 
-	for (int i=0;i<class_labels.size();i++) {
-		string label = class_labels[i];
-		float val[1] = {i};
-		samples.push_back(classes_training_data[label]);
-		Mat class_label = Mat(classes_training_data[label].rows, 1, CV_32FC1, val);
+	for (map<string,Mat>::iterator it = classes_training_data.begin(); it != classes_training_data.end(); ++it) {
+		string class_ = (*it).first;
+		cout << "#" << num++ << " " << class_ << "..." << endl;
+		
+		Mat samples(0,response_hist.cols,response_hist.type());
+		Mat labels(0,1,CV_32FC1);
+		
+		//copy class samples and label
+		samples.push_back(classes_training_data[class_]);
+		Mat class_label = Mat::ones(classes_training_data[class_].rows, 1, CV_32FC1);
 		labels.push_back(class_label);
+
+		//copy rest samples and label
+		for (map<string,Mat>::iterator it1 = classes_training_data.begin(); it1 != classes_training_data.end(); ++it1) {
+			string not_class_ = (*it1).first;
+			if(not_class_[0] == class_[0]) continue;
+			samples.push_back(classes_training_data[not_class_]);
+			class_label = Mat::zeros(classes_training_data[not_class_].rows, 1, CV_32FC1);
+			labels.push_back(class_label);
+		}
+		
+		Mat samples_32f; 
+		samples.convertTo(samples_32f, CV_32F);
+
+		/*
+		Ptr<CvSVM> tmp_svm = new CvSVM();
+		CvSVMParams params = CvSVMParams();
+		params.svm_type    = CvSVM::C_SVC;
+		auto lastInsertPtr = classes_classifiers.insert(make_pair(class_, tmp_svm));
+		lastInsertPtr.first->second->train(samples_32f,labels,Mat(),Mat(),params);
+		*/
+		
+		Ptr<CvSVM> tmp_svm = new CvSVM();
+		auto lastInsertPtr = classes_classifiers.insert(make_pair(class_, tmp_svm));
+		lastInsertPtr.first->second->train(samples_32f,labels);
+		
 	}
-
-	samples.convertTo(samples_32f, CV_32F);
-	svm.train(samples_32f,labels,Mat(),Mat(),params);
-
-	svm.save("vcom_svm.xml");
-
-	//cout << "Training classes..." << endl;
-
-	////train 1-vs-all SVMs
-	//map<string, Ptr<CvSVM>> classes_classifiers;
-	//int num = 1;
-
-	//for (map<string,Mat>::iterator it = classes_training_data.begin(); it != classes_training_data.end(); ++it) {
-	//	string class_ = (*it).first;
-	//	cout << "#" << num++ << " " << class_ << "..." << endl;
-	//	
-	//	Mat samples(0,response_hist.cols,response_hist.type());
-	//	Mat labels(0,1,CV_32FC1);
-	//	
-	//	//copy class samples and label
-	//	samples.push_back(classes_training_data[class_]);
-	//	Mat class_label = Mat::ones(classes_training_data[class_].rows, 1, CV_32FC1);
-	//	labels.push_back(class_label);
-
-	//	//copy rest samples and label
-	//	for (map<string,Mat>::iterator it1 = classes_training_data.begin(); it1 != classes_training_data.end(); ++it1) {
-	//		string not_class_ = (*it1).first;
-	//		if(not_class_[0] == class_[0]) continue;
-	//		samples.push_back(classes_training_data[not_class_]);
-	//		class_label = Mat::zeros(classes_training_data[not_class_].rows, 1, CV_32FC1);
-	//		labels.push_back(class_label);
-	//	}
-	//	
-	//	Mat samples_32f; 
-	//	samples.convertTo(samples_32f, CV_32F);
-
-	//	/*
-	//	Ptr<CvSVM> tmp_svm = new CvSVM();
-	//	CvSVMParams params = CvSVMParams();
-	//	params.svm_type    = CvSVM::C_SVC;
-	//	auto lastInsertPtr = classes_classifiers.insert(make_pair(class_, tmp_svm));
-	//	lastInsertPtr.first->second->train(samples_32f,labels,Mat(),Mat(),params);
-	//	*/
-	//	
-	//	Ptr<CvSVM> tmp_svm = new CvSVM();
-	//	auto lastInsertPtr = classes_classifiers.insert(make_pair(class_, tmp_svm));
-	//	lastInsertPtr.first->second->train(samples_32f,labels);
-	//	
-	//}
 
 	cout << endl;
 
@@ -295,16 +218,13 @@ int main( int argc, char** argv ) {
 				cout << "Unable to detect any keypoints for this image." << endl;
 				continue;
 			}
-			
-			//bowide.compute(img, keypoints, response_hist);
-			bowide->compute(img, keypoints, response_hist);
 
-			/*for (map<string, Ptr<CvSVM>>::iterator it = classes_classifiers.begin(); it != classes_classifiers.end(); ++it) {
+			bowide.compute(img, keypoints, response_hist);
+
+			for (map<string, Ptr<CvSVM>>::iterator it = classes_classifiers.begin(); it != classes_classifiers.end(); ++it) {
 				int res = (*it).second->predict(response_hist,false);
 				cout << "class: " << (*it).first << ", response: " << res << endl;
-			}*/
-			int res = svm.predict(response_hist);
-			cout << "class: " << class_labels[res];
+			}
 		} 
 
 	} else { //DIRECTORY MODE
@@ -333,9 +253,7 @@ int main( int argc, char** argv ) {
 			img = imread(full_file_name);
 			keypoints.clear();
 			detector.detect(img, keypoints);
-			//bowide.compute(img, keypoints, response_hist);
-			bowide->compute(img, keypoints, response_hist);
-		
+			bowide.compute(img, keypoints, response_hist);
 
 			for (map<string, Ptr<CvSVM>>::iterator it = classes_classifiers.begin(); it != classes_classifiers.end(); ++it) {
 				int res = (*it).second->predict(response_hist,false);
